e4a76fe6c5d3f316184f83156fc22913ae621f83
Loreferr <lf.95@outlook.com>
2023-05-17 11:01:45 +0200

    SearchPage

--- Javascript/searchbar_script.js
+++ Javascript/searchbar_script.js
@@ -1,63 +1,66 @@
-let searchButton = document.getElementById("searchForJs")
+let searchButton = document.getElementById("searchForJs");
 
-let searchBar = document.getElementById("searchBar")
+let searchBar = document.getElementById("searchBar");
 
 // parte distruzione
-let mainColumn = document.querySelectorAll("main div.container-fluid");
+let mainColumn = document.querySelector(".primoContainer");
 searchButton.addEventListener("click", () => {
-    mainColumn.forEach((element) => {
-      element.classList.add("destruction");
-    });
-  });
-
+  mainColumn.classList.add("destruction");
+});
 
 //per rimuover d-none alla searchbar
-searchButton.addEventListener("click",()=>{
-    
-    searchBar.classList.toggle("d-none")
-})
+searchButton.addEventListener("click", () => {
+  searchBar.classList.toggle("d-none");
+});
 
 //logica get artist
-let searchBarText = document.getElementById("searchBarText")
-console.log("🚀 ~ file: searchbar_script.js:11 ~ searchBarText:", searchBarText)
+let searchBarText = document.getElementById("searchBarText");
+console.log(
+  "🚀 ~ file: searchbar_script.js:11 ~ searchBarText:",
+  searchBarText
+);
 
-// searchBarText.addEventListener("keydown",(e)=>{
-
-// if (e.key === "Enter"){
-//     console.log(searchBarText.value);
-//     fetch(`https://striveschool-api.herokuapp.com/api/deezer/artist/${searchBarText.value}`)
-//     .then((res)=>{
-//         if(res.ok){
-//             console.log("tutto ok");
-//             return res.json()
-//         }else{
-//             throw new Error("qualcosa è andato storto")
-//         }
-//     })
-//     .then((obj)=>{
-//         console.log(obj);
-//         //ci ritorna l'artista
-//         backgroundimgArtist(obj);
-//         createLikedSection(obj);
-//         fetch(obj.tracklist)
-//         .then((tracklist)=>{
-//             if(tracklist.ok){
-//                 return tracklist.json()
-//             }else{
-//                 throw new Error("qualcosa è andato storto")
-//             }
-//         })
-//         .then((canzoni)=>{
-//             console.log("canzoni dell artista",canzoni);
-//             let tracks = canzoni.data;
-//             console.log("🚀 ~ file: searchbar_script.js:38 ~ .then ~ tracks:", tracks)
-//             // QUI DEVI LAVORARE CON L ARRAY
-//             destructionMain();
-//             populatePopularSong(tracks);
-//         })
-//         .catch((err)=>
-//         console.log(err))
-//     })
-//     .catch((err)=>console.log(err))
-// }
-// })
+searchBarText.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    console.log(searchBarText.value);
+    fetch(
+      `https://striveschool-api.herokuapp.com/api/deezer/artist/${searchBarText.value}`
+    )
+      .then((res) => {
+        if (res.ok) {
+          console.log("tutto ok");
+          return res.json();
+        } else {
+          throw new Error("qualcosa è andato storto");
+        }
+      })
+      .then((obj) => {
+        console.log(obj);
+        //ci ritorna l'artista
+        backgroundimgArtist(obj);
+        createLikedSection(obj);
+        fetch(obj.tracklist)
+          .then((tracklist) => {
+            if (tracklist.ok) {
+              return tracklist.json();
+            } else {
+              throw new Error("qualcosa è andato storto");
+            }
+          })
+          .then((canzoni) => {
+            console.log("canzoni dell artista", canzoni);
+            let tracks = canzoni.data;
+            console.log(
+              "🚀 ~ file: searchbar_script.js:38 ~ .then ~ tracks:",
+              tracks
+            );
+            // QUI DEVI LAVORARE CON L ARRAY
+            destructionMain();
+            populatePopularSong(tracks);
+            console.log("funziona tutto");
+          })
+          .catch((err) => console.log(err));
+      })
+      .catch((err) => console.log(err));
+  }
+});
